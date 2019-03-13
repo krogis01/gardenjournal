b208d64fbfbf837e16665582c384fc9758812456
Withers <Withers@Witherss-iMac.local>
2019-03-12 22:18:16 -0500

    added input and submit to first page, making request for JSON object to retrieve zone

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import request = require('request-promise-native');
 
 @Component({
   selector: 'app-home',
@@ -6,5 +7,20 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  public user_zip_code: any;
 
+  sendRequest() {
+    const zoneUrl = `https://c0bra.api.stdlib.com/zipcode-to-hardiness-zone/?zipcode=${this.user_zip_code}`;
+    let returnedHtml = '';
+    // alert(zoneUrl);
+
+    request(zoneUrl).then(function(htmlString) {
+      returnedHtml = htmlString;
+    }).catch(function(err) {
+      returnedHtml = 'Sorry, we could not find your zone';
+    });
+
+    alert(returnedHtml);
+    return returnedHtml;
+  }
 }
