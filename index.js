215a79d505d37da00eed4b336e2eca36c782c151
Withers <Withers@Witherss-iMac.local>
2019-09-01 12:03:17 -0500

    find plant start dates for indoor vegetables

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const readline = require('readline');
-var rpn = require("request-promise-native");
+const rpn = require("request-promise-native");
+const vegetableInfo = require('./vegetableInfo.json');
   
 const getHardinessZone = async(zip) => {
   let zone;
@@ -26,10 +27,10 @@ const getFrostDates = async(zip) => {
           const table = body.match(`<table>(.*)<\/table>`)[0];
           const tableValues = JSON.stringify(table).match(/<td>([A-Za-z ,0-9']+)<\/td>/g);
           frostDateInfo = {
-              "Nearest Climate Station": tableValues[0].slice(4, -5),
-              "Last Spring Frost": tableValues[2].slice(4, -5),
-              "First Fall Frost": tableValues[3].slice(4, -5),
-              "Growing Season": tableValues[4].slice(4, -5)
+              "nearest_climate_station": tableValues[0].slice(4, -5),
+              "last_spring_frost": tableValues[2].slice(4, -5),
+              "first_fall_frost": tableValues[3].slice(4, -5),
+              "growing_season": tableValues[4].slice(4, -5)
           }
       } else {
           console.log(error);
@@ -51,6 +52,46 @@ rl.question('What is your zip code?', async(answer) => {
 
   const frostDates = await getFrostDates(answer);
   console.log(`Your frost information is: ${JSON.stringify(frostDates)}`);
+
+  const month = frostDates.last_spring_frost.slice(0,3);
+  const date = frostDates.last_spring_frost.slice(-2);
+
+  const months = {
+    "Jan": 0,
+    "Feb": 1,
+    "Mar": 2,
+    "Apr": 3,
+    "May": 4,
+    "Jun": 5,
+    "Jul": 6,
+    "Aug": 7,
+    "Sep": 8,
+    "Oct": 9,
+    "Nov": 10,
+    "Dec": 11
+  };
+
+  let springFrostDate = new Date();
+  springFrostDate.setMonth(months[month]);
+  springFrostDate.setDate(date);
+
+  console.log(`Last Spring Frost: ${springFrostDate}`);
+  let startIndoorsDates = [];
+
+  Object.keys(vegetableInfo).map((vegetable) => {
+    if (vegetableInfo[vegetable].indoors.days_before_frost.length > 1) {
+      vegetableInfo[vegetable].indoors.days_before_frost.map((dateOptions) => {
+        const vegetableDate = new Date(springFrostDate);
+        const newDate = vegetableDate.getDate() - dateOptions;
+        vegetableDate.setDate(newDate);
+        startIndoorsDates.push(vegetableDate);
+    
+        return vegetableDate;
+      });
+    
+      console.log(`You should start your ${vegetable} between ${startIndoorsDates[0]} and ${startIndoorsDates[1]}`);
+    }
+  });
   
   rl.close();
 });
